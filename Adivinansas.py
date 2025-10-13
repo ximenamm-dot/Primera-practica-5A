7878d7f838069e88f4a3a1dd07767b2536d0e374
ximenamm-dot <ximenamm@isb.edu.mx>
2025-10-13 15:00:30 +0000

    se guarda la  practica

---
+++ Adivinansas.py
@@ -0,0 +1,20 @@
+imort random
+
+#Paso 1 : crear la lista de adivinanzas(Preguntas, respuestas)
+
+adivinanzas=(
+    ("Blanco por dentro, verde por fuera. Si quieres que te lo diga,espera."," pera"),
+    ("Oro parece, plata no es.¿ Qué es","platano"),
+    ("Tengo haujas pero no coso, tengo numeros pero no soy matematico.¿Qué soy?","reloj"),
+    ("Tiene dientes pero no muerde.¿Qué es?","peine"),
+    ("Cuanto más seca, más moja,¿Que es?","toalla"),
+    ("Tiene cuello pero no caseza.¿Que es?","Botella"),
+    ("Tiene ojos pero no ve.¿Que es?","aguja"),
+    ("Sube y baja pero. no se mueve.¿Que es?","escalera"),
+    ("Tiene corona pero no es rey.¿Que es?","piña")
+    
+
+# Paso 2 : Definir los puntos iniciales
+puntos=50
+
+print("✨¡Bienbenidos al juego de adivinanzas!✨")
